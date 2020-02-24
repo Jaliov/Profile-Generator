159d72c6eb6c4c37659faeea07db600628a83817
Joel Rudin <jarud55@gmail.com>
2020-02-24 06:29:17 -0500

    display tweak

--- index.js
+++ index.js
@@ -80,8 +80,8 @@ function genMD({userName, userEmail, project, description, licenseType, tests,  
 
     console.log("project",project);
     return `
-##${userEmail}
-##${userName}
+##${"User Email", userEmail}
+##${"User Name", userName}
 # ${project}
 ## Description
 ${description}
